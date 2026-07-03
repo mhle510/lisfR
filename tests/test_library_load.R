0c0480c6729f7157c521564d724ad6b0629a9275
Manh-Hung Le <55498716+mhle510@users.noreply.github.com>
2026-07-03 00:43:20 -0400

    Update test_library_load.R

--- tests/test_library_load.R
+++ tests/test_library_load.R
@@ -1,2 +1,6 @@
 devtools::load_all()
 ls("package:lisfR")   # or use search() to confirm lisfR is attached via load_all
+
+exists("nc_to_raster")
+exists("raster_to_nc")
+exists("get_nc_global_attrs")
